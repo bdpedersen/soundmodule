67bed9a6663be83f9fcaeae3f91792abc0af3d6d
Brian Pedersen <brian.dam.pedersen@gmail.com>
2025-06-06 17:36:05 +0200

    Another fix

--- build.rs
+++ build.rs
@@ -6,6 +6,5 @@ fn main() {
         .join("include")
         .join("soundmodule.h");
 
-    println!("cargo::rerun-if-changed={}", header_path.display());
     println!("cargo::rustc-env=DEP_SOUNDMODULE_INCLUDE={}", header_path.parent().unwrap().display());
 }
